76de894706d0297cf7a7bd57ce80c884f18c7c79
Ken Bellows <ken.bellows@live.com>
2015-02-02 14:38:19 -0500

    initialized with 'yo angular'

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tieShirtApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tieShirtApp
+ */
+angular.module('tieShirtApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
